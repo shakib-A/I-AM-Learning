492bd1fe227d9359da56da3d860437bf9e5601bd
unknown <avesta9312@gmail.com>
2022-09-02 16:30:37 +0430

    how to add function to the SHOW PASSWORD checbox

---
+++ controling forms/main.js
@@ -0,0 +1,9 @@
+const input = document.getElementById('pass')
+
+function showPassword(){
+    if(input.type === 'password'){
+        input.type = 'text'
+    } else {
+        input.type = 'password'
+    }
+}
