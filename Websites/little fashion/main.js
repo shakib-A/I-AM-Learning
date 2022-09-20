fe95d7267a6cbf0b598d31476287606337de0984
unknown <avesta9312@gmail.com>
2022-09-20 13:27:06 +0430

    completed

--- Websites/little fashion/main.js
+++ Websites/little fashion/main.js
@@ -1,7 +1,11 @@
 const toggleButton = document.querySelector('.toggle-button')
 const navLinks = document.querySelector('.nav-links-media')
 const navBar = document.querySelector('.nav-bar')
+//carousel inputs
+const slide = document.querySelectorAll('.slide')
+const trackerButtons = document.querySelectorAll('.tracker-btn')
 
+// menu toggle
 toggleButton.addEventListener('click',() => {
 
     if(navLinks.classList.contains('active')){
@@ -28,5 +32,41 @@ window.addEventListener('scroll', () => {
 })
 
 
+// carousel
+trackerButtons.forEach(element => {
+    element.addEventListener('click',  nextSlide)
+});
 
+// functions
+function nextSlide() {
+    //remove the current classes
+    removeCurrentBtn()
+    removeCurrentSlide()
+    // add current btn
+    this.classList.add('current-btn')
+    //add current slide
+    const currentSlide = document.querySelector(`#${this.id}-slide`)
+    currentSlide.classList.add('current-slide')
+}
 
+function removeCurrentBtn() {
+    trackerButtons.forEach(button => button.classList.remove('current-btn'))
+}
+
+function removeCurrentSlide() {
+    slide.forEach(slide => slide.classList.remove('current-slide'))
+}
+
+const myInterval = setInterval(function myFun() {
+    const CurrentBtn = document.querySelector('.current-btn')
+    const CurrentSlide = document.querySelector('.current-slide')
+    removeCurrentSlide()
+    removeCurrentBtn()
+    if(CurrentBtn.id == "third") {
+        slide[0].classList.add('current-slide')
+        trackerButtons[0].classList.add('current-btn')
+    } else {
+        CurrentBtn.nextElementSibling.classList.add('current-btn')
+        CurrentSlide.nextElementSibling.classList.add('current-slide')
+    }
+}, 5000)
