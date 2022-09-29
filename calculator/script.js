43299e46bced2a64958e01bcf7a1e14c203fc482
unknown <avesta9312@gmail.com>
2022-09-29 11:21:54 +0330

    completed

--- calculator/script.js
+++ calculator/script.js
@@ -1,60 +1,112 @@
-class Calculator {
-    constructor(dispOne, dispTwo) {
-        this.dispOne = dispOne
-        this.dispTwo = dispTwo
+class Calcualtor {
+    constructor(current, last){
+        this.current = currentDisplay
+        this.last = lastDisplay
+        this.claerAll()
+    }
+    claerAll(){
+        this.current.innerText = ""
+        this.last.innerText = ""
+        this.previousNumber = undefined
+        this.operation = undefined
     }
 
-    clearAll() {
-      this.dispTwo.innerHTML = ""
-      this.dispOne.innerHTML = ""
+    getNumVal(number){
+        this.current.innerText += number
     }
 
-    sumUp() {
-
+    deletLast(){
+        this.current.innerHTML = this.current.innerHTML.slice(0,-1)
     }
 
-    minus() {
+    operate(operand) {
+        this.compute()
+        // this if statement updates the operation anytime you click a new operation
+        if(this.last.innerHTML !== "" && this.operation !== operand){
+            this.last.innerText = this.last.innerText.slice(0,-1) + operand
+            this.operation = operand
+        }
 
+        // stops you from adding more than one operation
+        if(this.last.innerHTML.includes(operand)) return
+
+        // variables for later computation
+        this.previousNumber = parseInt(this.current.innerText);
+        this.operation = operand;
+
+        this.last.innerText += ` ${this.current.innerText} ${operand}`
+        this.current.innerText = ""
     }
 
-    operate() {
+    compute() {
+        if(this.current.innerText !== "" && this.last.innerText !== ""){
+            this.currentNumber = parseInt(this.current.innerText)
+            switch(this.operation){
+                case "+":
+                    // update the last display
+                    this.current.innerText = `${this.previousNumber + this.currentNumber}`
+                    //update the previousnumber
+                    this.previousNumber =  this.previousNumber + this.currentNumber
+                    this.last.innerText = ""
+                    break
 
-    }
+                case "-":
+                    this.current.innerText = `${this.previousNumber - this.currentNumber}`
+                    this.previousNumber = this.previousNumber - this.currentNumber
+                    this.last.innerText = ""
+                    break
+                    
+                case "×":
+                    this.current.innerText = `${this.previousNumber * this.currentNumber}`
+                    this.previousNumber = this.previousNumber * this.currentNumber
+                    this.last.innerText = ""
+                    break
 
-    updateDisplays() {
-        if(dispTwo) {
-
+                case "÷":
+                    this.current.innerText = `${this.previousNumber / this.currentNumber}`
+                    this.previousNumber = this.previousNumber / this.currentNumber
+                    this.last.innerText = ""
+                    break
+            }
         }
     }
 }
 
-const numberButton = document.querySelectorAll('.item')
-const equalButton = document.querySelector('.equals')
-const plusButton = document.querySelector('.plus')
-const minusButton = document.querySelector('.minus')
-const acButton = document.querySelector('.ac')
-const dispOne = document.querySelector('#input-1')
-const dispTwo = document.querySelector('#input-2')
+const currentDisplay = document.querySelector('.display-2')
+const lastDisplay = document.querySelector('.display-1')
+const numberButtons = document.querySelectorAll('#num')
+const operatorButtons = document.querySelectorAll('.operator')
+const deleteButton = document.querySelector('.delete')
+const clearButton = document.querySelector('.clear-all')
+const equalButton = document.querySelector('.equal')
 
+// creating a claculator class
+const calculator = new Calcualtor(currentDisplay, lastDisplay)
 
-const calculate = new Calculator(dispOne, dispTwo)
-console.log(calculate)
+// click events
+clearButton.addEventListener('click',() => {
+    calculator.claerAll()
+})
 
-for(let i = 0; i < numberButton.length; i++) {
-    numberButton[i].addEventListener('click', function numberClicked(){
-        dispTwo.innerHTML += numberButton[i].value
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.getNumVal(button.value)
     })
-}
+})
 
-plusButton.addEventListener('click', () => {
-    calculate.updateDisplays()
+deleteButton.addEventListener('click', () => {
+    calculator.deletLast()
+})
+
+operatorButtons.forEach(operator => {
+    operator.addEventListener('click', () => {
+        calculator.operate(operator.innerText)
+    })
 })
 
 equalButton.addEventListener('click', () => {
-    calculate.operate()
-    calculate.updateDisplays()
+    calculator.compute()
 })
 
-acButton.addEventListener('click', () => {
-    calculate.clearAll() 
-})
+
+
