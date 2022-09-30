1da80455d2f5843cd9c1d9ab880647012b42dca9
unknown <avesta9312@gmail.com>
2022-09-30 21:57:30 +0330

    test folder

--- this will be deleted/main.js
+++ this will be deleted/main.js
@@ -1,22 +1,17 @@
-document.querySelector('#btn').addEventListener('click',addUser)
-
-function addUser() {
-    const xhr =new XMLHttpRequest()
-    xhr.open('GET', "users.json", true)
-    xhr.onload = function(){
-            var users = JSON.parse(this.responseText)
-        var randomUser = Math.round(Math.random()*30)
-        output = ""
-        output += `<div class="container">
-                        <img src="${users[randomUser].avatar_url}">
-                        <ul>
-                            <li>ID: ${users[randomUser].id}</li>
-                            <li>UserName: ${users[randomUser].login}</li>
-                            <li>UserType: ${users[randomUser].type}</li>
-                        </ul>
-                   </div>`
-                   document.querySelector('body').innerHTML += output
+const myPromise = new Promise((UberIsHere, UberLost) => {
+    if(1 == 1){
+        UberIsHere(1)
+    } else {
+        UberLost(0)
     }
+})
 
-    xhr.send()
-}
+myPromise
+    .then((massage) => {
+        console.log(`the promise ${massage}`)
+    })
+    .catch((massage) => {
+        console.log(`the promise ${massage}`)
+    })
+
+console.log(myPromise)
