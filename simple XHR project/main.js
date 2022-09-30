997719660b56bf8e051035ba2547f8d1a0edd13d
unknown <avesta9312@gmail.com>
2022-09-30 21:56:55 +0330

    XHR project

---
+++ simple XHR project/main.js
@@ -0,0 +1,24 @@
+document.querySelector('#btn').addEventListener('click',addUser)
+
+function addUser() {
+    const xhr =new XMLHttpRequest()
+    xhr.open('GET', "users.json", true)
+    xhr.onload = function(){
+       if(xhr.status == 200) {
+        var users = JSON.parse(this.responseText)
+        var randomUser = Math.round(Math.random()*30)
+        output = ""
+        output += `<div class="container">
+                        <img src="${users[randomUser].avatar_url}">
+                        <ul>
+                            <li>ID: ${users[randomUser].id}</li>
+                            <li>UserName: ${users[randomUser].login}</li>
+                            <li>UserType: ${users[randomUser].type}</li>
+                        </ul>
+                   </div>`
+                   document.querySelector('body').innerHTML += output
+       }
+    }
+
+    xhr.send()
+}
