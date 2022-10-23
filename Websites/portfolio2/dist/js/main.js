982ca36f509a500929e7431accf1b2967e691c22
unknown <avesta9312@gmail.com>
2022-10-23 14:21:38 +0330

    a

--- Websites/portfolio2/dist/js/main.js
+++
@@ -1,7 +0,0 @@
-const navPage = document.querySelector('.nav-page')
-const navIcon = document.querySelector('.nav-icon')
-
-navIcon.addEventListener('click', () => {
-    navPage.classList.toggle('show')
-    navIcon.classList.toggle('close')
-})
